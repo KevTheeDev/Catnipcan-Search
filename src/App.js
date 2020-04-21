b90eee713b1f4b362126264267843c56980a3b66
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-21 12:21:28 -0500

    cleared, manifest json (icon things), navi....bar up, app.js and everything rendering fine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import NavigationBar from './components/NavigationBar.js'
 
+
 import { Route, BrowserRouter as Router, Link, Switch, } from 'react-router-dom';
 
 class App extends React.Component {
   state = {  }
   render() { 
     return ( 
-      <NavigationBar />
+      <Router>
+        <div className="App">  
+          <NavigationBar />
+          {/* <Route exact path="/" component={Home} />  */}
+          {/* <Route path="/Bookmarks" component={Bookmarks} />
+          <Route exact path="/UserProfile" component={UserProfile} />   */}
+        </div>
+      </Router>
      );
   }
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import '../src/components/NavigationBar.css';
+import React from 'react'; 
+import './NavigationBar.css'
 import { Route, BrowserRouter as Router, Link, Switch, } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <nav> 
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
 
-          <li>
-            <Link to="/Bookmarks">Bookmarks </Link>
-          </li>
+        <div className="navList">
+            <nav> 
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
 
-          <li>
-            <Link to="/UserProfile">UserProfile</Link>
-          </li>
+                    <li>
+                        <Link to="/Bookmarks">Bookmarks </Link>
+                    </li>
 
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
+                    <li>
+                        <Link to="/UserProfile">UserProfile</Link>
+                    </li>
 
-        </ul>
-      </nav>
-    </div>
+                    <li>
+                        <Link to="/Login">Login</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
   );
 }
 
