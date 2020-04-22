23aec4358c4c94a1c19a585829f018d461f16bb9
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-22 10:11:22 -0500

    starting fetch func

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,18 @@ import './Home.css'
 import { Route, BrowserRouter as Router, Link, Switch, } from 'react-router-dom';
 
 function Home(){
+
+    const fetchCats = () => {
+
+    }
+
     return(
         <div className="hmpg" >
             <h1> Home Page  </h1>
             <div>
                 <p> All Your Furry Cuddly Needs </p>
                 {/* insert the  api here /> */}
+                
             </div>
         </div>
     )
