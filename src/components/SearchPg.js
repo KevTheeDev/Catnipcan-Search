19e820837c06942272385d11d2a55f9e056e107b
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-22 09:55:42 -0500

    searchpg.js has some content in it he tag

---
+++ src/components/SearchPg.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+import { Route, BrowserRouter as Router, Link, Switch, } from 'react-router-dom';
+
+function Home(){
+    return(
+        <div className="hmpg" >
+            <h1> Search Page  </h1>
+            <div>
+                {/* insert the  api here /> */}
+            </div>
+        </div>
+    )
+}
+ 
+export default Home;
+ 
