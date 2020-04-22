bb6a3d3dacc957fadad3b16d37f88b20ccdb619d
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-22 10:23:59 -0500

    added us effectfunc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css'
 import { Route, BrowserRouter as Router, Link, Switch, } from 'react-router-dom';
 
 function Home(){
 
-    const fetchCats = () => {
+    //useEffect func came with vscode or some sort of extension
+    //({}) <- means it only runs when the components mount
+    useEffect(() => {
+        //return statement ()
+        fetchCats ();
+        }
+    }, [])
 
+    const fetchCats = async () => {
+        const data = await fetch ('https://thecatapi.com/v1/images?api_key=aaa32b32-301f-40ad-8c62-a9054e33d188')
+        console.log(data);
     }
 
     return(
