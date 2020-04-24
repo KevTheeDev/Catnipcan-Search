518337ccf5fce2b5a0b3c3fa2bbea89ba6cc16ba
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-24 01:15:22 -0500

    made all listed cat names into links

--- src/components/Home.js
+++ src/components/Home.js
@@ -46,7 +46,7 @@ class Home extends React.Component{
                         {/* map ( => <li> To Return The List Item (in this case, for the catNames) <li>) */}
                         {/* ul is neede around <li> in order for a list to be created */}
                         <h3> Instructions please click the dropdown menu below or in the upper left corner of the page and search for a cat! </h3>
-                        <Link to={`/`}>  </Link>
+                        <Link to={`/SearchPg/`}> <h4> {this.state.data.map((catNames, id) => <ul key={id}> <li key={id} > {catNames.name} </li> </ul>)} </h4> </Link>
                         <h4> {this.state.data.map((catNames, id) => <ul key={id}> <li key={id} > {catNames.name} </li> </ul>)} </h4>
                     
             </div>
